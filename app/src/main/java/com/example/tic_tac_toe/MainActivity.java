31a66e648dd01078faf880836987fb03a2e9ab96
sachin <sachin.aralapura@gmail.com>
2023-04-19 16:26:49 +0530

    initial commit

---
+++ app/src/main/java/com/example/tic_tac_toe/MainActivity.java
@@ -0,0 +1,42 @@
+package com.example.tic_tac_toe;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+    // 0 - x
+    // 1 - o
+    int activePlayer = 1;
+    int gameState[] = {2,2,2,2,2,2,2,2,2};
+    // state  meaning
+    // 0 - x
+    // 1 - o
+    // 2 - null
+    int[][] winPositions = {{0,1,2},{3,4,5},{6,7,8},{0,3,6},{1,4,7 },{2,5,8},{0,4,8},{2,4,6}};
+    public  void  playerTap(View view){
+        ImageView img = (ImageView) view;
+        int tappedImage = Integer.parseInt(img.getTag().toString());
+        if(gameState[tappedImage] == 2){
+            gameState[tappedImage] = activePlayer;
+            img.setTranslationY(-1000);
+            if(activePlayer == 0) {
+                img.setImageResource(R.drawable.x);
+                activePlayer = 1;
+            }
+            else {
+                img.setImageResource(R.drawable.o);
+                activePlayer = 0;
+            }
+        }
+    }
+
+    @Override
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
